b344740ddcbce720d1744a4b7216a87d015a10a6
AICHHORN181 <=>
2020-08-10 14:01:46 +0200

    Add project setup

---
+++ src/main/java/com/aichhorn/AnimalShelter.java
@@ -0,0 +1,29 @@
+package com.aichhorn;
+
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class AnimalShelter extends Application{
+
+    public void showLoginScreen(Stage stage) {
+        GridPane grid = new GridPane();
+
+        Label userName = new Label("Benutzername: ");
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
